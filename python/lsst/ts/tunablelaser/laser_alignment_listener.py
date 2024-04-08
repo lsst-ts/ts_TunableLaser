7ba401373bffe3a3dd5c10256aa00e9c484c3b41
Joseph Parsons <parsonsjosephb@gmail.com>
2024-04-07 18:32:01 -0600

    Styling changes and stuff

--- python/lsst/ts/tunablelaser/laser_alignment_listener.py
+++ python/lsst/ts/tunablelaser/laser_alignment_listener.py
@@ -1,3 +1,29 @@
+# This file is part of ts_tunablelaser.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site Software.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+""" This a script for the raspberry pi running alongside the TunableLaser CSC
+    This script's purpose is to listen for 1kHz laser misalignment and
+    activate an interlock
+"""
+
 __all__ = ["LaserAlignmentListener", "execute_laser_alignment_listener"]
 
 import asyncio
@@ -17,10 +43,35 @@ RELAY_OFF = 0
 
 
 def execute_laser_alignment_listener():
+    """Run the laser alignment task"""
     asyncio.run(LaserAlignmentListener.amain(index=None))
 
 
 class LaserAlignmentListener(tcpip.OneClientServer):
+    """This is the class that implements the laser alignment script.
+
+    Parameters
+    ----------
+    logger : `logging.Logger`
+        logger object
+    port : `int`, optional
+        port that the server will be hosted on, default 1883
+    host : `str`, optional
+        IP the server will be hosted on, default tcpip.DEFAULT_LOCALHOST
+    encoding : `str`, optional
+        Encoding used for the packets
+    terminator: `bytes`, optional
+        terminating character used for packets
+    sample_record_dur: `float`, optional
+        sample recording duration
+    input: `int`, optional
+        index of sd device input
+    output: `int`, optional
+        index of sd device output
+    fs: `int`, optional
+        sample frequency
+    """
+
     def __init__(
         self,
         logger: logging.Logger,
@@ -28,9 +79,10 @@ class LaserAlignmentListener(tcpip.OneClientServer):
         host: str | None = tcpip.DEFAULT_LOCALHOST,
         encoding: str = tcpip.DEFAULT_ENCODING,
         terminator: bytes = tcpip.DEFAULT_TERMINATOR,
-        sample_wait_time=60,
-        serial=None,
-        temperature_windows=8,
+        sample_record_dur: float = 0.1,
+        input: int = 2,
+        output: int = 4,
+        fs: int | None = None,
     ):
         super().__init__(
             log=logger,
@@ -49,21 +101,32 @@ class LaserAlignmentListener(tcpip.OneClientServer):
         self.fileptr = None
         self.pending_messages = []
 
-        self.config()
+        self.input = input
+        self.output = output
+        self.sample_record_dur = sample_record_dur
 
-    def config(self):
+        sd.default.device = (input, output)
+        if fs is None:
+            self.fs = sd.query_devices(input)["default_samplerate"]
+        else:
+            self.fs = fs
+
         self.relay_gpio = 7
         self.configured = True
         self.pi = pigpio.pi()
 
     async def amain(self):
-        self.laser_alignment_task()
+        """Script amain"""
+        self.open_laser_interrupt()
+        self.laser_alignment_task(self.sample_record_dur, self.fs)
 
     def publish_msg(self, msg):
+        """Function that adds msg to be sent to pending msgs"""
         self.pending_messages.append(msg)
         self.read_and_dispatch()
 
     def read_and_dispatch(self):
+        """Will pop off pending messages and send them"""
         # read data
         # incoming_data = self.read_str()
         # handle data
@@ -73,8 +136,9 @@ class LaserAlignmentListener(tcpip.OneClientServer):
             self.write_str(self.pending_messages.pop() + self.terminator)
 
     def record_data(self, duration, fs):
+        """Records sample data from sd device"""
         self.logger.debug("Check input settings")
-        sd.check_input_settings(device=input, samplerate=fs, channels=1)
+        sd.check_input_settings(device=self.input, samplerate=fs, channels=1)
         self.logger.debug(f"Starting to record for {duration} seconds")
         data = sd.rec(
             frames=int(duration * fs), samplerate=fs, channels=1, blocking=True
@@ -82,6 +146,7 @@ class LaserAlignmentListener(tcpip.OneClientServer):
         return data
 
     def analyze_data(self, data, fs):
+        """analyzes all sound data and determines if there is a problem"""
         # average the tracks
         # a = (data.T[0] + data.T[1])/2.0
         a = data.T[0]
@@ -156,7 +221,7 @@ class LaserAlignmentListener(tcpip.OneClientServer):
         plt.draw()
         plt.pause(0.001)
 
-    def set_relay(self, setting):
+    def set_relay(self, setting: int):
         if not self.configured and not self.pi.connected:
             self.publish_msg(
                 "LI: Error: Not configured properly before actuating relay"
@@ -190,7 +255,11 @@ class LaserAlignmentListener(tcpip.OneClientServer):
         # when it's able to propagate
         return not self.pi.read(self.relay_gpio)
 
-    def laser_alignment_task(self, time, fs):
+    def laser_alignment_task(self, time: float | None = None, fs: float | None = None):
+        if time is None:
+            time = self.sample_record_dur
+        if fs is None:
+            fs = self.fs
         try:
             self.logger.info("Starting monitoring task")
 
@@ -202,7 +271,6 @@ class LaserAlignmentListener(tcpip.OneClientServer):
             count_threshold = 7  # 10
             count = 0
 
-            # TODO remove debug csv
             self.fileptr = "{}/{}_{}.csv".format(
                 FILEFOLDER, FILENAME, str(datetime.now().date())
             )
