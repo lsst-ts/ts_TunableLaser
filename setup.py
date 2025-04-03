87cd1218fd1f7c069d32ad519dfbb8b5f1d9dcbd
Eric Coughlin <ecoughlin@lsst.org>
2025-04-03 17:12:01 -0300

    fixup! Make unstable simulation behavior second simulation mode.

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ import setuptools_scm
 setuptools.setup(
     version=setuptools_scm.get_version(
         write_to="python/lsst/ts/tunablelaser/version.py",
-        relative_to="pyproject.toml",
     )
 )
